16f276dc33d5f90b69c215fa1b043ef573bc66bc
Neeraj Kumar <neerajkumar.xyz@gmail.com>
2019-10-19 18:00:20 +0530

    first spring project is CRM

---
+++ CustomerRelationshipManagement/src/test/java/com/madhu/crm/CustomerRelationshipManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhu.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRelationshipManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
